f13cb532d3d75ad10dd51c9f35637cb1eb64f7e0
Monica Costa <monicacosta@usf.edu>
2019-01-15 17:56:14 -0500

    Version 2

--- HelloName/Program.cs
+++ HelloName/Program.cs
@@ -6,7 +6,10 @@ namespace HelloName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "Costa Monica";
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine(name);
+            Console.ReadKey(true);
         }
     }
 }
